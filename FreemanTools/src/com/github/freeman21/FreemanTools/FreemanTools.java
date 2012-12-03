e790a38e2bd407cce0d54456bf0d4e3ce49b4afc
Freeman21 <Freeman@Freeman-PC>
2012-12-03 23:08:25 +0100

    Updated to 1.1
    changelog:	- changed slaptool to config(default bread)
    
    Updated to 1.0
    
    changelog:	- blockhead (/blockhead)
    			- added config file support

--- FreemanTools/src/com/github/freeman21/FreemanTools/FreemanTools.java
+++ FreemanTools/src/com/github/freeman21/FreemanTools/FreemanTools.java
@@ -7,6 +7,8 @@
 package com.github.freeman21.FreemanTools;
 
 import java.util.HashMap;
+import java.util.Map;
+import java.util.Map.Entry;
 import java.util.Random;
 import java.util.logging.Logger;
 
@@ -15,6 +17,10 @@ import net.milkbowl.vault.permission.Permission;
 
 import org.bukkit.Location;
 import org.bukkit.Material;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+import org.bukkit.configuration.ConfigurationSection;
+import org.bukkit.enchantments.Enchantment;
 import org.bukkit.entity.Creeper;
 import org.bukkit.entity.Entity;
 import org.bukkit.entity.Player;
@@ -38,7 +44,6 @@ import org.bukkit.util.Vector;
 
 public class FreemanTools extends JavaPlugin implements Listener {
 	
-	public static final int velocityFactor = 5;
 	private Permission permission = null;
 	@SuppressWarnings("unused")
 	private Logger log = Logger.getLogger("FreemanTools");
@@ -46,8 +51,25 @@ public class FreemanTools extends JavaPlugin implements Listener {
 
 	public void onEnable(){
 		setupPermissions();
+		setConfig(null);
 		getServer().getPluginManager().registerEvents(this, this);
-		this.saveDefaultConfig();
+	}
+	
+	public void setConfig(FreemanTools FreemanTools){
+		//this.saveDefaultConfig();
+		final ConfigurationSection config = getConfig();
+		final Map<String, Object> def = new HashMap<String, Object>();
+		def.put("Freemanslap.FallVelocity", 5);
+		def.put("Freemanslap.SlapTool", 297);
+		def.put("Freemanslap.Enable.Other", true);
+		def.put("Freemanslap.Enable.Self", true);
+		def.put("FreemanHeadDrop.Enable", true);
+		def.put("FreemanHeadDrop.DropChance", 5);
+		def.put("FreemanBlockHead.Enable", true);
+		for (final Entry<String, Object> e : def.entrySet())
+            if (!config.contains(e.getKey()))
+                    config.set(e.getKey(), e.getValue());
+		saveConfig();
 	}
 	
 	private boolean setupPermissions()
@@ -58,30 +80,23 @@ public class FreemanTools extends JavaPlugin implements Listener {
         }
         return (permission != null);
     }
-	
-	/**
-	 * Check wherether the specified player has the specified permission node.
-	 * 
-	 * @param player the player to check.
-	 * @param node the permission node to check.
-	 * 
-	 * @return true if the player has the specified permission.
-	 */
+
 	public boolean has(Player player, String node) {
-		return permission != null && permission.has(player, "freemantools." + node);
+		return permission != null && permission.has(player, "freemantools." + node	);
 	}
 	
 	@EventHandler
 	public void playerHit(EntityDamageEvent subevent) {
+		if(FreemanTools.this.getConfig().getBoolean("Freemanslap.Enable.Other")){
 		if(!(subevent instanceof EntityDamageByEntityEvent)) return;
 		EntityDamageByEntityEvent event = (EntityDamageByEntityEvent) subevent;
 		if(!(event.getDamager() instanceof Player)) return;
-		if(!(event.getEntity() instanceof Player)) return;
+		//if(!(event.getEntity() instanceof Player)) return;
 		
 		Player player = (Player) event.getDamager();
-		Player target = (Player) event.getEntity();
+		Entity target = event.getEntity();
 		
-		if(player.getItemInHand() == null || player.getItemInHand().getTypeId() != Material.STICK.getId()) return;
+		if(player.getItemInHand() == null || player.getItemInHand().getTypeId() != FreemanTools.this.getConfig().getInt("Freemanslap.SlapTool")) return;
 		if(!has(player, "slap.other")) return;//check if player has permission freemantools.slap.other
 		
 		PlayerInventory inventory = player.getInventory();
@@ -92,40 +107,100 @@ public class FreemanTools extends JavaPlugin implements Listener {
 		else{
 			target.setFireTicks(10000);
 		}
-		player.sendMessage("you have slapped " + target.getDisplayName() + "!");
-		target.sendMessage("you were slapped by " + player.getDisplayName() + "!");
+		player.sendMessage("you have slapped a "+ event.getEntityType());
+		//target.sendMessage("you were slapped by " + player.getDisplayName() + "!");
 		//getLogger().info("Player + hit");
-		Vector velocity = player.getLocation().getDirection().multiply(velocityFactor);
+		Vector velocity = player.getLocation().getDirection().multiply(FreemanTools.this.getConfig().getInt("Freemanslap.FallVelocity"));
 		target.setVelocity(velocity);
 		event.setCancelled(true);
-	}
+		}
+	}   
 	
 	@EventHandler(ignoreCancelled = true,priority = EventPriority.HIGHEST)
-	public void selfHit(PlayerInteractEvent event){
+	public void selfHit(PlayerInteractEvent event) throws InterruptedException{
+		if(FreemanTools.this.getConfig().getBoolean("Freemanslap.Enable.Self")){
 		Player player = (Player) event.getPlayer();
 		if(event.getAction() != Action.LEFT_CLICK_BLOCK) return;
-		if(player.getItemInHand() == null || player.getItemInHand().getTypeId() != Material.STICK.getId()) return;
+		if(player.getItemInHand() == null || player.getItemInHand().getTypeId() != FreemanTools.this.getConfig().getInt("Freemanslap.SlapTool")) return;
 		if(!has(player, "slap.self")) return;//check if player has permission freemantools.slap.self
-		
 		player.sendMessage("you have slapped yourself!");
-		Vector velocity = player.getLocation().getDirection().multiply(velocityFactor * -0.4);
+		Vector velocity = player.getLocation().getDirection().multiply(FreemanTools.this.getConfig().getInt("Freemanslap.FallVelocity") * -0.4);
 		player.setVelocity(velocity);
 		event.setCancelled(true);
+		}
+	}
+	
+	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args){
+		if(cmd.getName().equalsIgnoreCase("blockhead")){ // If the player typed /basic then do the following...
+			if(FreemanTools.this.getConfig().getBoolean("FreemanBlockHead.Enable")){
+				Player player = (Player) sender;
+				if(!has(player, "blockhead")) return false;//check if player has permission freemantools.slap.other
+				PlayerInventory inventory = player.getInventory();
+				int handAmount = player.getItemInHand().getAmount();
+				short damagevalue = player.getItemInHand().getDurability();
+				int handleft = handAmount -=1;
+				int slot = inventory.getHeldItemSlot();
+				int item = player.getItemInHand().getTypeId();
+				Map<Enchantment, Integer> ench = inventory.getItemInHand().getEnchantments();
+				
+				if (handleft == 0){
+					ItemStack itemstack4 = new ItemStack(Material.AIR, handleft,damagevalue);
+					inventory.setItem(slot, itemstack4);
+					
+				}
+				else{
+					ItemStack itemstack4 = new ItemStack(item, handleft,damagevalue);
+					inventory.setItem(slot, itemstack4);
+					
+				}
+				
+				ItemStack itemstack3 = inventory.getHelmet();
+				ItemStack itemstack2 = new ItemStack(item, 1,damagevalue);
+				itemstack2.addEnchantments(ench);
+			
+				if (itemstack3 != null)
+					if (inventory.getHelmet() != null){
+						Map<Enchantment, Integer> enchHelmet = inventory.getHelmet().getEnchantments();
+					{
+					if (enchHelmet != null){
+						itemstack3.addEnchantments(enchHelmet);
+					}
+					player.getWorld().dropItemNaturally(player.getLocation(), itemstack3);
+					}
+				}
+				inventory.setHelmet(itemstack2);
+				player.sendMessage("You now have a new blockhead");
+				return true;
+			}else{
+				Player player = (Player) sender;
+				player.sendMessage("This is not enabled");
+			}
+			
+		}
+		if(cmd.getName().equalsIgnoreCase("FreemanToolsVersion")){
+			Player player = (Player) sender;
+			String version = getDescription().getVersion();
+			player.sendMessage("FreemanTools is on version " + version);
+			return true;
+		}
+		return false; 
 	}
 	
 	@EventHandler(ignoreCancelled = true)
 	
 	public void headDrop(EntityDeathEvent event)
 		{
+		if(FreemanTools.this.getConfig().getBoolean("FreemanHeadDrop.Enable")){
 		Player Killer = event.getEntity().getKiller();
 		Entity Target = event.getEntity();
 		Location Drop = Target.getLocation();
+
+		
 		if(Killer != null) 
 			{
 				Random randomGenerator = new Random();
 				int randomInt = randomGenerator.nextInt(100);
-			
-				if (randomInt >95 ){
+				if (randomInt < FreemanTools.this.getConfig().getInt("FreemanHeadDrop.DropChance") ){
 					if (Target instanceof Creeper)
 					{
 						ItemStack itemstack = new ItemStack(397, 1,(short)4);
@@ -149,6 +224,7 @@ public class FreemanTools extends JavaPlugin implements Listener {
 				}
 			}
 		}
+	}
 	@EventHandler (ignoreCancelled = true)
 	public void Freezeplayer(PlayerMoveEvent event){
 		
